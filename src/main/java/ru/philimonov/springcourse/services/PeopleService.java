e4774171a440009ddff9b265b0840a2f8fd429a9
Aleksandr Philimonov <aleksandrphilimonov@gmail.com>
2023-02-12 14:29:40 +0300

    Initial commit

---
+++ src/main/java/ru/philimonov/springcourse/services/PeopleService.java
@@ -0,0 +1,50 @@
+package ru.philimonov.springcourse.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import ru.philimonov.springcourse.models.Person;
+import ru.philimonov.springcourse.repositories.PeopleRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+/**
+ * @author Philimonov Aleksandr
+ */
+@Service
+@Transactional(readOnly = true)
+public class PeopleService {
+
+    private final PeopleRepository peopleRepository;
+
+    @Autowired
+    public PeopleService(PeopleRepository peopleRepository) {
+        this.peopleRepository = peopleRepository;
+    }
+
+    public List<Person> findAll() {
+        return peopleRepository.findAll();
+    }
+
+    public Person findOne(int id) {
+        Optional<Person> foundPerson = peopleRepository.findById(id);
+        return foundPerson.orElse(null);
+    }
+
+    @Transactional
+    public void save(Person person) {
+        peopleRepository.save(person);
+    }
+
+    @Transactional
+    public void update(int id, Person updatedPerson) {
+        updatedPerson.setId(id);
+        peopleRepository.save(updatedPerson);
+    }
+
+    @Transactional
+    public void delete(int id) {
+        peopleRepository.deleteById(id);
+    }
+}
